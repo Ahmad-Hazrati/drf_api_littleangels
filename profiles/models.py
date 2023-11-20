c9e575857dc5d24df074ad8eca52127d75144cf1
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-20 13:12:45 +0000

    Create signal and connect to function, register model to admin.py and create superuser

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -17,5 +18,12 @@ class Profile(models.Model):
         ordering = ['-created_at']
 
 
-def __str__(self):
-    return f"{self.user}'s profile"
+    def __str__(self):
+        return f"{self.user}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
