c233fe7dd07e919e9f3136d26c6bbdf74e9788f7
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-21 09:48:24 +0000

    Create followers app, add models and serializer and duplicate handling function to it

---
+++ followers/serializers.py
@@ -0,0 +1,26 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id',
+            'user',
+            'created_at',
+            'followed',
+            'followed_name',
+        ]
+
+    def create(self, validate_data):
+        try:
+            return super().create(validate_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail:' 'possible duplicate'
+                })
