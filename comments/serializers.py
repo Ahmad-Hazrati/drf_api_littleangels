ac77998c52cb0f2d602e5956241a25de1b742d66
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-21 20:46:08 +0000

    Add root route, pagination, JSON renderer and date time formating to the app

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -7,10 +8,18 @@ class CommentSerializer(serializers.ModelSerializer):
     is_user = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='user.profile.id')
     profile_image = serializers.ReadOnlyField(source='user.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    modified_at = serializers.SerializerMethodField()
 
     def get_is_user(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_modified_at(self, obj):
+        return naturaltime(obj.modified_at)
 
     class Meta:
         model = Comment
