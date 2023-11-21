a5d4d2acf5d344aeb42c388109aa70f478420ec0
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-21 19:50:18 +0000

    Add Comment filters using DjangoFilterBackend

--- profiles/views.py
+++ profiles/views.py
@@ -19,6 +19,7 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'user__following__followed__profile',
+        'user__followed__user__profile',
     ]
     ordering_fields = [
         'posts_count',
