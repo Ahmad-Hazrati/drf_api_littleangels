e1f06fec68da545a89ce74472585ddb3ee6da032
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-26 12:31:57 +0000

    Add PostCreateForm to components

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles/SignInUpForm.module.css";
-import btnStyles from "../styles/Button.module.css";
-import appStyles from "../App.module.css";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
 
 import {
   Form,
