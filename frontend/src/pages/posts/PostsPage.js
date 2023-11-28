54e1cb5d0bc71b94532adf8ec6271f3ada16cd71
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-28 12:47:57 +0000

    Install and add InifiniteScrolling to fetch more data as scrolled

--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -11,7 +11,9 @@ import NoResults from "../../assets/no-results.png";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -30,10 +32,12 @@ function PostsPage({ message, filter = "" }) {
         console.log(err);
       }
     };
+
     setHasLoaded(false);
     const timer = setTimeout(() => {
       fetchPosts();
     }, 1000);
+
     return () => {
       clearTimeout(timer);
     };
@@ -60,9 +64,15 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map((post) => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
