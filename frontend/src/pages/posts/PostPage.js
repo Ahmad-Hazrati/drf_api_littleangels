06924d979c91869621ea6e218b35de892e32a789
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-29 09:27:35 +0000

    Add Comment dropdown menu and create comment delete functionality

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -51,7 +51,12 @@ export const PostPage = () => {
           ) : null}
           {comments.results ? (
             comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
             <span>No comments yet, be the first one to comment!</span>
