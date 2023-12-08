552ae4c1e3576705ba2efaaf0f06457d7cfba8ed
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-12-08 12:00:46 +0000

    Update frontend static files for deployment

---
+++ staticfiles/build/precache-manifest.23f0381580b406217923ac778cfc51f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f122e3ee9c7cf180350ef64e0d7ee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76879e88fa0d124247d5",
+    "url": "/static/css/main.72a42bce.chunk.css"
+  },
+  {
+    "revision": "9265a5a6e10820e10327",
+    "url": "/static/js/2.cbf1f91d.chunk.js"
+  },
+  {
+    "revision": "f056774035ce39e6238293d1e83850f9",
+    "url": "/static/js/2.cbf1f91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f8b39ca8c9c9c055ff",
+    "url": "/static/js/3.7e9fb503.chunk.js"
+  },
+  {
+    "revision": "76879e88fa0d124247d5",
+    "url": "/static/js/main.fd66f7c8.chunk.js"
+  },
+  {
+    "revision": "d6004698e541a218d4a6",
+    "url": "/static/js/runtime-main.e156b0f9.js"
+  },
+  {
+    "revision": "5c7e5891d91a52331a0335de8ceff30f",
+    "url": "/static/media/logo.5c7e5891.png"
+  }
+]);
