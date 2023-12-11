4bf3d3e44bdfc6a06aa991ea1e2af3407442d9d7
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-12-11 21:23:55 +0000

    Add method to convert the time into human-readable format

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Booking
 
@@ -14,13 +15,30 @@ class BookingSerializer(serializers.ModelSerializer):
     is_user = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='user.profile.id')
     profile_image = serializers.ReadOnlyField(source='user.profile.image.url')
-    
+    created_at = serializers.SerializerMethodField()
+    modified_at = serializers.SerializerMethodField()
+
     def get_is_user(self, obj):
         """
-        Method to determine if the current request user is the creator of the booking.
+        Method to determine if the current request user is
+        the creator of the booking.
         """
         request = self.context['request']
         return request.user == obj.user
+
+    def get_created_at(self, obj):
+        """
+        Method to return a human-readable time difference
+        since the booking was created.
+        """
+        return naturaltime(obj.created_at)
+
+    def get_modified_at(self, obj):
+        """
+        Method to return a human-readable time difference
+        since the booking was last modified.
+        """
+        return naturaltime(obj.modified_at)
 
     class Meta:
         model = Booking
